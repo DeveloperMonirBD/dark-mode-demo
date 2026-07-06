0c34e8ae26d28bd4871bb42e44ca905078da6ca4
Monir <mrmonir0558@gmail.com>
2026-07-06 17:12:59 +0600

    updated navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,8 @@ import Navbar from './components/Navbar';
 export default function App() {
     return (
         <div
-            className="
-      min-h-screen
-      bg-gray-100
-      text-black
-
-      dark:bg-gray-900
-      dark:text-white
-
-      transition-all
-      duration-300
-    ">
+            className="min-h-screen bg-gray-100 text-black dark:bg-gray-900 dark:text-white transition-all duration-300">
             <Navbar />
-
             <div className="flex justify-center items-center mt-20">
                 <Card />
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { useTheme } from '../context/ThemeContext';
 import ThemeButton from './ThemeButton';
 
 export default function Navbar() {
+    const { theme } = useTheme();
+
     return (
         <nav className="flex justify-between items-center p-5 border-b">
-            <h2 className="text-2xl font-bold">Dark Mode Demo</h2>
+            <h2 className="text-2xl font-bold">{theme === 'dark' ? 'Dark Mode Demo' : 'Light Mode Demo'}</h2>
 
             <ThemeButton />
         </nav>
